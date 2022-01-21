844ca2ffcd9913e76fdde2330ace2a6bf81fa51d
LeeSoMyoung <so4644009@gmail.com>
2022-01-21 21:52:29 +0900

    chore: webpack config 파일 최종

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const miniCssExtract = require('mini-css-extract-plugin');
 
 module.exports={
     mode: 'development',
@@ -8,5 +9,22 @@ module.exports={
     output:{
         path: path.resolve(__dirname,'src','frontend','dist','js'),
         filename: '[name].js'
-    }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader',miniCssExtract.loader]
+            },
+            {
+                test:/\.png$/,
+                use:['file-loader']
+            },{
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use:['babel-loader']
+            }
+        ]
+    },
+    target:['web','es5']
 }
