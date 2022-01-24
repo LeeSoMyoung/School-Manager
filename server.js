20fb78831b37d7a03cf8ef55a824aa577345bdff
LeeSoMyoung <so4644009@gmail.com>
2022-01-24 21:44:11 +0900

    feat: JWT 관련 코드 일부 업데이트 프로젝트 로직에서 로그인이 되어 있으면 바로 메인 화면을 보여주고, 로그인이 되어있지 않다면 /login으로 가 로그인 화면을 보여주어야 한다. JWT에 로그인 정보를 저장하는 작업 수행

--- server.js
+++ server.js
@@ -6,15 +6,36 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const logger = require('morgan');
+const token = require('./src/backend/api/token');
+const jwt = require('jsonwebtoken');
 
 const PORT = process.env.PORT;
 
 app.use(logger('dev'));
+app.use(express.json());
 
 app.use('/static',express.static(path.resolve(__dirname,'src','frontend','static')));
+app.use('/backend',express.static(path.resolve(__dirname,'src','backend')));
 
 app.get('/*', (req,res)=>{
     res.sendFile(path.resolve('src','frontend','index.html'));
+});
+
+app.post('/login',(req,res)=>{
+   const id = req.body.id;
+   const pw = req.body.pw;
+   const user = {id:id, pw:pw};
+   const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
+
+   res.json({accessToken:accessToken});
+});
+
+app.get('/login',token.authenticationToken,(req,res)=>{
+    res.json({"id":req.user.id});
+});
+
+app.get('/',token.authenticationToken,(req,res)=>{
+    res.json({"현재 유저의 id":req.user.id});
 });
 
 app.listen(PORT||8081, ()=>{
