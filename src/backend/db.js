95e3494f70678c39fdaeb528fc5fc394415202e7
LeeSoMyoung <so4644009@gmail.com>
2022-02-08 19:37:54 +0900

    refactor: DB 관련 코드 리팩토링 및 디렉터리 추가
    server.js에 작성했던 DB 관련 코드를 src/backend/db.js에
    코드를 옮겼다. 또한, src/backend/middlewares라는 디렉터리를 만들어
    내부 users.js에서 유저 관련 로직을 처리할 수 있도록 하였다.

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,15 +1,23 @@
+'use strict';
+
 require('dotenv').config();
 
 const mysql = require('mysql');
-
-const DB_PASSWORD = process.env.DB_PASSWORD;
-
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:DB_PASSWORD,
-    database:'school_manager'
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE
 });
-connection.connect();
-// 쿼리 작성
-connection.end();
+
+db.connect((error)=>{
+    if(error){
+        // DB를 연결하지 못했다면 연결하지 못한 원인을 띄운다.
+        console.log(error);
+    }
+    else{
+        console.log('MySQL is Connected...');
+    }
+});
+
+module.exports = db;
